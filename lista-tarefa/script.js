23035febd78683d821ccc1a5c0ad733fd08d878f
Daniela Duarte <danyeladu@yahoo.com.br>
2022-08-29 03:46:16 -0300

    desafio

---
+++ lista-tarefa/script.js
@@ -0,0 +1,27 @@
+const inputTarefa = document.getElementById("tarefa-nova")
+
+function adicionarTarefa(){
+    const item = document.createElement("li")
+
+    item.innerHTML = `<li>
+    <span>${inputTarefa.value}</span>
+    <button class="excluir">X</button>
+    </li>`;
+
+    const lista = document.querySelector("#lista");
+
+    lista.appendChild(item);
+
+    inputTarefa.value=""
+
+}
+
+const botaoAdicionar = document.querySelector("#btn-adicionar")
+botaoAdicionar.addEventListener("click", adicionarTarefa)
+
+function limparLista(){
+    const lista = document.querySelector("#lista");
+    lista.innerhtHTML = ""
+}
+
+const botaoLimpar = document.addEventListener("click", limparLista)
